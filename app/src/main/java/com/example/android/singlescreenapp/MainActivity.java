91a5d8a013f750d7508effc022bb012173b66b05
daibeat <daibeat@github.com>
2018-02-23 16:00:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/singlescreenapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.singlescreenapp;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onClick(View view) {
+
+
+        Uri uri = Uri.parse("https://www.instagram.com/zona230/");
+
+
+        Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+        likeIng.setPackage("com.instagram.android");
+
+        try {
+            startActivity(likeIng);
+        } catch (ActivityNotFoundException e) {
+
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.instagram.com/zona230/")));
+        }
+    }
+}
